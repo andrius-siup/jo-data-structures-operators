cf9b770ab0b032cdb6eebe35f01f9be950d59918
andrius-siup <andrius.siupinys@gmail.com>
2022-01-06 21:13:26 +0000

    Destructuring nested array

--- script.js
+++ script.js
@@ -63,3 +63,10 @@ console.log(main, secondary);
 // Receive 2 return values from a function
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, j] = nested;
+// console.log(i, j); // 2 4
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k); // 2 5 6
