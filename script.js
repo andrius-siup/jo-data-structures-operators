508939fba525548b03986b2979192e26779fece4
andrius-siup <andrius.siupinys@gmail.com>
2022-01-11 12:32:43 +0000

    Created function add, that sum numbers, used REST spread variable

--- script.js
+++ script.js
@@ -70,6 +70,16 @@ const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays);
 
 // 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  // Was wrapped into {} and was diff print
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
 
 /*
 /////////////////////////////////
