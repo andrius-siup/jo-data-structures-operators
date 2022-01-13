bd0e6695fd077118387e4ff2a2043c664fd74904
andrius-siup <andrius.siupinys@gmail.com>
2022-01-13 18:55:11 +0000

    Maps - Fundamentals

--- script.js
+++ script.js
@@ -54,6 +54,42 @@ const restaurant = {
 };
 
 //////////////////////////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+// Example that current time is 21
+const time = 21; // change below 11- will be closed
+// Checked or restaurant are open
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2); //rm Lisbon, Portugal
+// rest.clear();  // clear all data
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/*
+//////////////////////////////////////////////////////
 // Sets- cannot be duplicate and no indexes, values unique
 const orderSet = new Set([
   'Pasta',
@@ -94,6 +130,8 @@ console.log(
 console.log(new Set(staff).size); // unique size
 
 console.log(new Set('antanasKavaliaskas').size); // 9 unique
+*/
+
 /*
 ///////////////////////////////////////////////////////////
 // Looping Objects: Keys, values, entries
