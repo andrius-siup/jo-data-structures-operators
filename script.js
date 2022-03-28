0235ca608ceb28bf21f3388caa915b8a660345b4
andrius-siup <andrius.siupinys@gmail.com>
2022-03-28 14:10:12 +0100

    Challenge 3 - initialize variable events to new Set

--- script.js
+++ script.js
@@ -55,44 +55,81 @@ const restaurant = {
 
 //////////////////////////////////////////////////////
 // Maps: Iteration
-const question = new Map([
-  ['question', 'What is the best programming language in the world'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct🎉'],
-  [false, 'Try again!'],
-]);
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct🎉'],
+//   [false, 'Try again!'],
+// ]);
 
-console.log(question);
+// console.log(question);
 
 // Convert Object to Map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
 
-console.log(hoursMap);
+// console.log(hoursMap);
 
 // Quiz app
-console.log(question.get('question'));
+// console.log(question.get('question'));
 
-for (const [key, value] of question) {
-  if (typeof key === 'number') {
-    console.log(`Answer ${key}: ${value}`);
-  }
-}
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') {
+//     console.log(`Answer ${key}: ${value}`);
+//   }
+// }
 
-// const answer = Number(prompt('Your answer'));
-const answer = 3;
-console.log(answer);
+// const answer = Number(prompt('Your answer')); not uncomment
 
-console.log(question.get(question.get('correct') === answer));
+// const answer = 3;
+// console.log(answer);
 
-// Convert Map to Array
-console.log([...question]);
-console.log([...question.entries()]);
-console.log([...question.keys()]);
-console.log([...question.values()]);
+// console.log(question.get(question.get('correct') === answer));
+
+// // Convert Map to Array
+// console.log([...question]);
+// console.log([...question.entries()]);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+// console.log([...gameEvents.values()]);
+
+// Set new Set
+const events = new Set(gameEvents.values());
+console.log(events);
 
 //////////////////////////////////////////////////////
 // Maps: Fundamentals
